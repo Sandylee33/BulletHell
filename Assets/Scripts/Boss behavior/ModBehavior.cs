db8398c08bec7fb342249d21b8a8b3e5c60f75d0
Haoyu Li <lihaoyu2678@gmail.com>
2017-08-14 18:22:52 -0700

    add mods

---
+++ Assets/Scripts/Boss behavior/ModBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mod : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		List<Mods> mods = new List<Mods> ();
+		for (int i = 0; i < 2; i++) {
+			Mods mod = new Mods (i);
+			mods.Add (mod);
+		}
+
+		mods[1].mod_desc = "single rotation";
+		mods[1].mod_rot = null;
+		mods[1].mod_bullet_move = null;
+
+		mods[2].mod_desc = "double rotation";
+		mods[2].mod_rot = null;
+		mods[2].mod_bullet_move = null;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Boss behavior/Mods.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mods {
+
+	public int mod_id;
+	public string mod_desc;
+	public rotation mod_rot;
+	public bulletmove mod_bullet_move;
+	//public Dictionary <int mod_id, Mods mod>();
+
+
+	public Mods(int id)
+	{
+		mod_id = id;
+	}
+
+
+//	public static Mods Switch(int id)
+//	{
+//		if (mod_id != id) 
+//		{
+//			Mods mod = new Mods (id);
+//			return mod;
+//		}
+//	}
+
+
+}
